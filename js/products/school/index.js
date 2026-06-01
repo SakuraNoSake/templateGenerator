df0a46df3b2991e8717a6e6690c03f7f76116876
Ruslan Nechaev <rus.nechaev01@mail.ru>
2026-06-01 16:16:48 +0300

    Добавил генератор ЛД для школ

--- js/products/school/index.js
+++ js/products/school/index.js
@@ -1,4 +1,62 @@
+import {initProduct} from "../../core/initProduct.js";
+import {SCHOOL_CONFIG} from "./config/constants.js";
+import {generatePersonSchoolFile} from "./templates/person.js";
+import {isRequired} from "../../utils/validators.js";
+
 export function initSchool() {
-    console.log('Модуль Школа в разработке');
-    alert('Модуль "Школа" находится в разработке');
+    initProduct({
+        constants: SCHOOL_CONFIG,
+
+        elements: {
+            generateBtn: document.getElementById('generateBtnSchool'),
+            templateTypeSelect: document.getElementById('templateTypeSchool'),
+            loading: document.getElementById('loadingSchool'),
+
+            rowsCountInput: document.getElementById('rowsCountSchool'),
+            orgGuidInput: document.getElementById('schoolOrgGuid'),
+            groupNameInput: document.getElementById('schoolGroupName'),
+        },
+
+        fields: {
+            rowsCount: document.querySelector('[for="rowsCountSchool"]')?.parentElement,
+            orgGuid: document.querySelector('[for="schoolOrgGuid"]')?.parentElement,
+            groupName: document.querySelector('[for="schoolGroupName"]')?.parentElement,
+        },
+
+        generators: {
+            personSchool: generatePersonSchoolFile,
+        },
+
+        validators: {
+            personSchool: (data) => {
+                if(!isRequired(data.orgGuid)) return alert('Введите CUID организации'), false
+                if(!isRequired(data.groupName)) return alert('Введите название класса/группы'), false
+                return true;
+            }
+        },
+
+        collectData: (el) => ({
+            templateType: el.templateTypeSelect.value,
+            rowsCount: Number(el.rowsCountInput.value),
+            orgGuid: el.orgGuidInput.value,
+            groupName: el.groupNameInput.value,
+        }),
+
+        buildArgs: (data) => {
+            switch (data.templateType) {
+                case 'personSchool':
+                    return [
+                        data.rowsCount,
+                        data.orgGuid,
+                        data.groupName
+                    ];
+                case 'groupSchool':
+                    return [
+                        data.rowsCount,
+                        data.orgGuid,
+                        data.groupName
+                    ];
+            }
+        }
+    })
 }
